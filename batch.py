236b022a68c98a3a3d7e40a1c9c8c859287764c3
Syrine <ben.attig.syrine@gmail.com>
2025-11-27 23:24:57 +0100

    staging tables created

--- batch.py
+++ batch.py
@@ -18,6 +18,7 @@ def ingest_games_wide():
     query = """
     SELECT *
     FROM `bigquery-public-data.baseball.games_wide`
+    LIMIT 50
     """
 
     logging.info("Running query on public dataset...")
@@ -50,6 +51,7 @@ def ingest_games_post_wide():
     query = """
     SELECT *
     FROM `bigquery-public-data.baseball.games_post_wide`
+    LIMIT 50
     """
 
     logging.info("Running query on public dataset...")
@@ -80,6 +82,7 @@ def ingest_schedules():
     query = """
     SELECT *
     FROM `bigquery-public-data.baseball.schedules`
+    LIMIT 50
     """
 
     logging.info("Running query on public dataset...")
@@ -103,7 +106,6 @@ def ingest_schedules():
     load_job.result() 
     logging.info(f"Data loaded successfully into {table_id}.")
     print(f"✅ Batch ingestion completed at {datetime.now()}")
-
 
 def ingest_cleaned_games_post_wide():
     client = bigquery.Client()
@@ -135,8 +137,9 @@ def ingest_cleaned_games_post_wide():
         awayFinalRuns AS away_final_runs,
         awayFinalHits AS away_final_hits,
         awayFinalErrors AS away_final_errors
-        FROM `bigquery-public-data.baseball.games_post_wide`
+        FROM `redsox-bq-project.raw_data.games_post_wide`
         WHERE gameId IS NOT NULL
+        LIMIT 50
         """
 
     logging.info("Running CREATE OR REPLACE TABLE ...")
@@ -146,6 +149,79 @@ def ingest_cleaned_games_post_wide():
     logging.info("Table refreshed successfully.")
     print("✅ staging.cleaned_games_post_wide updated.")
 
+def ingest_cleaned_games_wide():
+    client = bigquery.Client()
+
+    query = """
+    CREATE OR REPLACE TABLE `redsox-bq-project.staging.cleaned_games_wide` AS
+    SELECT
+        gameId AS game_id,
+        seasonId AS season_id,
+        seasonType AS season_type,
+        year,
+        CAST(startTime AS TIMESTAMP) AS start_time,
+        gameStatus AS game_status,
+        attendance,
+        dayNight AS day_night,
+        durationMinutes AS duration_minutes,
+        homeTeamId AS home_team_id,
+        homeTeamName AS home_team_name,
+        awayTeamId AS away_team_id,
+        awayTeamName AS away_team_name,
+        venueId AS venue_id,
+        venueName AS venue_name,
+        venueCity AS venue_city,
+        venueState AS venue_state,
+        venueCapacity AS venue_capacity,
+        homeFinalRuns AS home_final_runs,
+        homeFinalHits AS home_final_hits,
+        homeFinalErrors AS home_final_errors,
+        awayFinalRuns AS away_final_runs,
+        awayFinalHits AS away_final_hits,
+        awayFinalErrors AS away_final_errors
+        FROM `redsox-bq-project.raw_data.games_wide`
+        WHERE gameId IS NOT NULL
+        LIMIT 50
+        """
+
+    logging.info("Running CREATE OR REPLACE TABLE ...")
+    query_job = client.query(query)
+    query_job.result()
+
+    logging.info("Table refreshed successfully.")
+    print("✅ staging.cleaned_games_wide updated.")
+
+def ingest_cleaned_schedules():
+    client = bigquery.Client()
+
+    query = """
+    CREATE OR REPLACE TABLE `redsox-bq-project.staging.cleaned_schedules` AS
+    SELECT
+    gameId AS game_id,
+    gameNumber AS game_number,
+    seasonId AS season_id,
+    year,
+    type AS game_type,
+    dayNight AS day_night,
+    duration_minutes,
+    homeTeamId AS home_team_id,
+    homeTeamName AS home_team_name,
+    awayTeamId AS away_team_id,
+    awayTeamName AS away_team_name,
+    CAST(startTime AS TIMESTAMP) AS start_time,
+    attendance,
+    status
+    FROM `redsox-bq-project.raw_data.schedules`
+    WHERE gameId IS NOT NULL
+    LIMIT 50
+    """
+
+    logging.info("Running CREATE OR REPLACE TABLE ...")
+    query_job = client.query(query)
+    query_job.result()
+
+    logging.info("Table refreshed successfully.")
+    print("✅ staging.cleaned_schedules updated.")
 
 schedule.every().day.at("00:00").do(ingest_games_wide)
 schedule.every().day.at("00:00").do(ingest_games_post_wide)
@@ -156,6 +232,8 @@ if __name__ == "__main__":
     ingest_games_post_wide()
     ingest_schedules()
     ingest_cleaned_games_post_wide()
+    ingest_cleaned_games_wide()
+    ingest_cleaned_schedules()
 
 #while True:
 #    schedule.run_pending()
